fe8e3146c053d1162335e6d8f95d2f19cb0e4bb2
AlexeiKonev <alexkonev777@gmail.com>
2024-03-23 21:30:58 +0300

    health bar

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -10,6 +10,8 @@ namespace herogame
         private int _health = 1000;
 
         private int _damage;
+        [SerializeField]
+        private float _speed = 40f;
 
 
         private int _coins;
@@ -18,6 +20,8 @@ namespace herogame
         private RectTransform _healthBar;
 
         private CharacterController _characterController;
+        float x;
+        float y;
         private void Start()
         {
             _characterController = GetComponent<CharacterController>();
@@ -35,9 +39,9 @@ namespace herogame
 
         }
 
-		private void  Move()
+		private void  Move(float xVector , float yVector)
 		{
-            _characterController.SimpleMove(new Vector3(Input.GetAxis("Horizontal"),transform.position.y, Input.GetAxis("Vertical")));
+            _characterController.SimpleMove(new Vector3( xVector * _speed*Time.deltaTime,transform.position.y, yVector * _speed * Time.deltaTime));
 		}
 		// Update is called once per frame
 		private void Update()
@@ -47,9 +51,16 @@ namespace herogame
                 Die();
 			}
 
-            Move();
-        } 
-        private void LateUpdate()
+              x = Input.GetAxis("Horizontal");
+              y = Input.GetAxis("Vertical");
+
+           
+        }
+		private void FixedUpdate()
+		{
+            Move(x, y);
+        }
+		private void LateUpdate()
         {
             MoveHealthBar();
         }
